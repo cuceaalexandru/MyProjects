49a875a555e266f3f22c87899a435aca273c3346
Alex Cucea <43235041+cuceaalexandru@users.noreply.github.com>
2019-01-09 12:11:11 +0200

    Update main.py

--- ONI/Client/main.py
+++ ONI/Client/main.py
@@ -1,12 +1,14 @@
 import urllib2 #library used for getting page content
 import requests #library to make requests
+import socket# used to get hostname
 
 #Get webpage content
-#resp = urllib2.urlopen('http://localhost/ONI/index.php')
+#resp = urllib2.urlopen('http://localhost/ONI/index.php?')
 #page = resp.read()
 #print page
 
 #Make post request ex:send ID
-params = {'name': 'John','email': 'John.doe@example.com'}
-response = requests.get('http://localhost/ONI/index.php', params=params)
+hostname = socket.gethostname() #sending bot id
+params = {'id': hostname,'email': 'John.doe@example.com', "submit" : "OK"}
+response = requests.post('http://localhost/ONI/cmd.php', params=params)
 print response.content
