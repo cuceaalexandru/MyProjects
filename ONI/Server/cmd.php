387787406b2860139ab4a1903fda33bc97236347
Alex Cucea <43235041+cuceaalexandru@users.noreply.github.com>
2019-01-09 12:11:43 +0200

    Update cmd.php

--- ONI/Server/cmd.php
+++ ONI/Server/cmd.php
@@ -1,8 +1,22 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="id">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_REQUEST['id'];
+    if (empty($name)) {
+        echo "ID is empty";
+    } else {
+        echo $name;
+    }
+}
+?>
 
 </body>
 </html>
